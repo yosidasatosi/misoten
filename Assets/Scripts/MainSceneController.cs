b0a90059f9b058c4b2613b3afb0b0bda25367743
yosida <yosidasatosihal@gmail.com>
2019-10-16 22:52:29 +0900

    初期コミット

---
+++ Assets/Scripts/MainSceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneController : MonoBehaviour
+{
+    public string NextSceneName;
+    public Animation StageNameAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeInOut.Instance.FadeIn(1.0f,() => StageNameAnim?.Play());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(NextSceneName);
+        }
+    }
+}
